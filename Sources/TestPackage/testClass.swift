0a1451123211f5abfd2a659a31d722fbe6bd94e8
kawang <kawang@microstrategy.com>
2019-09-16 18:41:52 +0800

    package first commit

---
+++ Sources/TestPackage/testClass.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by kai on 9/16/19.
+//
+
+import Foundation
+
+public class testClass {
+    public init() {
+    }
+
+    open func printHello() {
+        print("Hello world!")
+    }
+}
